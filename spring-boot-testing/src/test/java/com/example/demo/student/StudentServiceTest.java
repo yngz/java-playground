0033e337573acbeeb1af4fc6fd1aaa0da716b77d
Yang Zou <yang0201@outlook.com>
2023-01-15 23:37:10 +0000

    Test service with mockito

---
+++ spring-boot-testing/src/test/java/com/example/demo/student/StudentServiceTest.java
@@ -0,0 +1,39 @@
+package com.example.demo.student;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class StudentServiceTest {
+
+    @Mock
+    private StudentRepository studentRepository;
+    private StudentService underTest;
+
+    @BeforeEach
+    void setUp() {
+        underTest = new StudentService(studentRepository);
+    }
+
+    @Test
+    void testGetAllStudents() {
+        //when
+        underTest.getAllStudents();
+        //then
+        verify(studentRepository).findAll();
+
+    }
+
+    @Test
+    void testAddStudent() {
+    }
+
+    @Test
+    void deleteStudent() {
+    }
+}
