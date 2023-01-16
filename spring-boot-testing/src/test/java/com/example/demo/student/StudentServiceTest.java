15973ff73160876bb25d6118e752469c3de4e1cf
Yang Zou <yang0201@outlook.com>
2023-01-16 20:03:22 +0000

    Test add student with ArgumentCaptor

--- spring-boot-testing/src/test/java/com/example/demo/student/StudentServiceTest.java
+++ spring-boot-testing/src/test/java/com/example/demo/student/StudentServiceTest.java
@@ -3,9 +3,11 @@ package com.example.demo.student;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
@@ -31,6 +33,24 @@ class StudentServiceTest {
 
     @Test
     void testAddStudent() {
+        //given
+        Student student = new Student(
+                "Jamila",
+                "Jamila@gmail.com",
+                Gender.FEMALE
+        );
+
+        //when
+        underTest.addStudent(student);
+
+        //then
+        ArgumentCaptor<Student> studentArgumentCaptor =
+                ArgumentCaptor.forClass(Student.class);
+        verify(studentRepository)
+                .save(studentArgumentCaptor.capture());
+
+        Student captured = studentArgumentCaptor.getValue();
+        assertThat(captured).isEqualTo(student);
     }
 
     @Test
