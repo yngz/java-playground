f2b0af5e83f55e5074934137053b88bc81138944
Yang Zou <yang0201@outlook.com>
2023-01-22 16:14:18 +0000

    Log in with in-memory user details

--- spring-security-basics/src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ spring-security-basics/src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -1,13 +1,27 @@
 package com.example.demo.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -18,5 +32,19 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails nezukoKamadoUser = User.builder()
+                .username("Nezuko Kamado")
+                .password(passwordEncoder.encode("password"))
+                .roles("STUDENT")  // ROLE_STUDENT
+                .build();
+
+        return new InMemoryUserDetailsManager(
+                nezukoKamadoUser
+        );
     }
 }
