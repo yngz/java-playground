fdde4ef54d9768aa23f78580a7167b28b11bbb32
Yang Zou <yang0201@outlook.com>
2023-02-05 21:15:04 +0000

    Remember me

--- spring-security-basics/src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ spring-security-basics/src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -13,6 +13,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import java.util.concurrent.TimeUnit;
+
 import static com.example.demo.security.ApplicationUserRole.*;
 
 @Configuration
@@ -39,7 +41,9 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .formLogin()
                 .loginPage("/login").permitAll()
-                .defaultSuccessUrl("/courses", true);
+                .defaultSuccessUrl("/courses", true)
+                .and()
+                .rememberMe().tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(21)); // defaults to 2 weeks
     }
 
     @Override
